7d194fae5bcf09a5046aa19f698efcc4c0de15ce
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-22 11:41:39 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,15 @@
 # Use this space to code along with the readme
+def hello
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+end
